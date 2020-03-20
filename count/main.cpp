96547d59d807c1e9de087def62acde0c6105b12e
shengkai <Administrator@DESKTOP-539KRNO>
2020-03-20 15:57:50 +0800

    main更改

--- count/main.cpp
+++ count/main.cpp
@@ -1,5 +1,6 @@
 #include<opencv2/opencv.hpp>
 #include<iostream>
+#include<math.h>
 
 using namespace cv;
 using namespace std;
@@ -7,6 +8,7 @@ using namespace std;
 int main() {
 	Mat src, binaryMat, labelMat, stateMat, centrMat, result;
 	src = imread("D:/coin.png");
+	cvtColor(src, src, CV_BGR2GRAY);
 	cv::threshold(src, binaryMat, 0, 255, THRESH_OTSU);
 
 	//获得连通域
@@ -21,13 +23,29 @@ int main() {
 	}
 
 	cout << "总的连通域个数" << com - 1 << endl;
-	//绘制box
+	result = cv::Mat::zeros(src.size(), CV_8UC3);
+	std::vector<cv::Vec3b>colors(com);
+	colors[0] = cv::Vec3b(0, 0, 0);
+	for (int i = 1; i < com; i++) {
+		colors[i] = cv::Vec3b(rand()/255, rand() / 255, rand() / 255);
+
+	}
+
+	for (int i = 0; i < src.rows; i++) {
+		for (int j = 0; j < src.cols; j++) {
+			int label = labelMat.at<int>(j, i);
+			result.at<cv::Vec3b>(j, i) = colors[label];
+		}
+	}
+
 	for (int i = 1; i < com; i++) {
 		Rect box;
 		box.x = stateMat.at<int>(i, 0);
 		box.y = stateMat.at<int>(i, 1);
 		box.width = stateMat.at<int>(i, 2);
 		box.height = stateMat.at<int>(i, 3);
+		rectangle(result, box, CV_RGB(255, 255, 255),1,8,0);
+
 
 	}
 	namedWindow("binaryMat", CV_WINDOW_AUTOSIZE);
@@ -36,15 +54,7 @@ int main() {
 	imshow("binaryMat", binaryMat);
 	imshow("result", result);
 	imshow("src", src);
-	moveWindow("src", 0, 20);
-	moveWindow("binaryMat", src.cols, 20);
-	moveWindow("result", src.cols * 2, 20);
-
-
-
+	
 	waitKey(0);
-
-
-
 
 }
